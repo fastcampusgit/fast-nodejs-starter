cf7e3f6b1607b672adf3daa774388e055273965a
Dongsu Jang <iolothebard@gmail.com>
2019-12-23 15:37:25 +0900

    replace mocha,chai,nyc into jest

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,12 +1,9 @@
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-
 const { greetings } = require('../dist/hello');
 
 describe('hello', () => {
   describe('greetings', () => {
     it('should be hello,world!', () => {
-      expect(greetings('world')).to.equal('hello,world!');
+      expect(greetings('world')).toEqual('hello,world!');
     });
   });
 });
--- test/hi.test.js
+++ test/hi.test.js
@@ -1,12 +1,9 @@
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-
 const { greetings } = require('../dist/hi');
 
 describe('hi', () => {
   describe('greetings', () => {
     it('should be hi,world!', () => {
-      expect(greetings('world')).to.equal('hi,world!');
+      expect(greetings('world')).toEqual('hi,world!');
     });
   });
 });
